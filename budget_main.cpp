b66adf82176cde668a127af6fda5086c907e8213
Stephen Petrick <stephenp1912@gmail.com>
2020-09-18 01:25:48 -0400

    trying to commit

--- budget_main.cpp
+++ budget_main.cpp
@@ -6,10 +6,88 @@ using namespace std; //according to a stack overflow post, using this is bad pra
                      // need to do std::string and etc if not using tho. will keep it for time being for convenience
 
 
-void writeToFile (string money, int day, int month, int year);
-void readFromFile (string fileName);
+//void writeToFile (string money, int day, int month, int year);
+//void readFromFile (string fileName);
+
+
+
+void writeToFile (string money, int day, int month, int year) {
+
+   string moneyBuffer = money;
+   string moneyBufferPureNumber = money;
+   //for some reason won't work using an iterator
+/*   for (auto i=begin(moneyBuffer); i!=end(moneyBuffer); i++){ 
+      if (moneyBuffer[*i] == ','){
+         moneyBuffer[*i] = '*';
+         cout << "Replaced comma" << endl;
+      }
+   }
+*/
+   //change commas from input to astrisk to prevent parsing conflict in CSV file
+   for (int i=0; i<moneyBuffer.length();  i++){ 
+         if (moneyBuffer[i] == ','){
+            moneyBuffer[i] = '*';
+            cout << "Replaced comma" << endl;
+         }
+   }
+
+   cout << "Parsed moneyBuffer: " << moneyBuffer << endl;
+
+   cout << "moneyBufferPureNumber vanilla: " << moneyBufferPureNumber << endl;
+   for (int i=0; i<moneyBufferPureNumber.length();  i++){ 
+         if (moneyBufferPureNumber[i] == ','){
+            cout << "Value to remove:" << moneyBufferPureNumber[i] << " Removed comma" << endl;
+            moneyBufferPureNumber.erase(moneyBufferPureNumber.begin()+i);
+         }
+   }
+   cout << "moneyBufferPureNumber: " << moneyBufferPureNumber << endl;
+   size_t idx=0;
+   double moneyDouble = stod(moneyBufferPureNumber, &idx);
+   cout << "moneyDouble: " << moneyDouble << endl;
+
+//Writing only line and writing over it. Potentially useful for debugging so DON'T DELETE
+	//ofstream write;
+	//write.open("test_file.csv");
+   ////CSV FORMAT: place value marked number (astrisk in place of commas), pure number (only decimal as symobl), day (number), month (number), year
+	//write << moneyBuffer << "," << moneyBufferPureNumber << "," << day << "," << month << "," << year << endl; 
+	//write.close();
+
+//Append new entry to file. I don't really know how it works though. Like why is the ifstream needed?
+   ofstream fout;
+   ifstream fin;
+   fin.open("test_file.csv");
+   fout.open("test_file.csv", ios::app);
+   if (fin.is_open()) {
+      fout << moneyBuffer << "," << moneyBufferPureNumber << "," << day << "," << month << "," << year << endl; 
+   }
+   fin.close();
+   fout.close();
+
+}
+
+void readFromFile (string fileName){
+
+   string buffer;
+   cout << "Data in file " << fileName << ":" << endl;
+//Read only one line of file. May be useful for debugging so DON'T DELETE
+//   ifstream read;
+//   read.open(fileName);
+//   read >> buffer;
+//Reads multiple lines from file and prints them
+   fstream read;
+   read.open(fileName, ios::in);
+   if (read.is_open()){
+      while (getline(read, buffer)){
+         cout << buffer << endl;
+      }
+   }
+   read.close();
+   cout << "\nFinished." << endl;
+}
+
 
 int main() {
+//CSV FORMAT: place value marked number (astrisk in place of commas), pure number (only decimal as symobl), day (number), month (number), year
 
 //	int date [8]; //not sure if possible to do with array
 	int day = 0, month = 0, year = 0;
@@ -31,19 +109,19 @@ int main() {
 	cout << "Success." << endl;
    return 0;
 }
-
+/*
 void writeToFile (string money, int day, int month, int year) {
 
    string moneyBuffer = money;
-   string moneyBuffer2 = money;
+   string moneyBufferPureNumber = money;
    //for some reason won't work using an iterator
-/*   for (auto i=begin(moneyBuffer); i!=end(moneyBuffer); i++){ 
-      if (moneyBuffer[*i] == ','){
-         moneyBuffer[*i] = '*';
-         cout << "Replaced comma" << endl;
-      }
-   }
-*/
+//   for (auto i=begin(moneyBuffer); i!=end(moneyBuffer); i++){ 
+//      if (moneyBuffer[*i] == ','){
+//         moneyBuffer[*i] = '*';
+//         cout << "Replaced comma" << endl;
+//      }
+//   }
+//
    //change commas from input to astrisk to prevent parsing conflict in CSV file
    for (int i=0; i<moneyBuffer.length();  i++){ 
          if (moneyBuffer[i] == ','){
@@ -54,16 +132,16 @@ void writeToFile (string money, int day, int month, int year) {
 
    cout << "Parsed moneyBuffer: " << moneyBuffer << endl;
 
-   cout << "moneyBuffer2 vanilla: " << moneyBuffer2 << endl;
-   for (int i=0; i<moneyBuffer2.length();  i++){ 
-         if (moneyBuffer2[i] == ','){
-            cout << "Value to remove:" << moneyBuffer2[i] << " Removed comma" << endl;
-            moneyBuffer2.erase(moneyBuffer2.begin()+i);
+   cout << "moneyBufferPureNumber vanilla: " << moneyBufferPureNumber << endl;
+   for (int i=0; i<moneyBufferPureNumber.length();  i++){ 
+         if (moneyBufferPureNumber[i] == ','){
+            cout << "Value to remove:" << moneyBufferPureNumber[i] << " Removed comma" << endl;
+            moneyBufferPureNumber.erase(moneyBufferPureNumber.begin()+i);
          }
    }
-   cout << "moneyBuffer2: " << moneyBuffer2 << endl;
+   cout << "moneyBufferPureNumber: " << moneyBufferPureNumber << endl;
    size_t idx=0;
-   double moneyDouble = stod(moneyBuffer2, &idx);
+   double moneyDouble = stod(moneyBufferPureNumber, &idx);
    cout << "moneyDouble: " << moneyDouble << endl;
 
 	ofstream write;
@@ -84,4 +162,4 @@ void readFromFile (string fileName){
    cout << "\nFinished." << endl;
    read.close();
 
-}
+}*/
