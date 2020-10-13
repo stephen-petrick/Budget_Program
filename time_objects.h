b27d5437911c30ceedc90e389fd3cf586e761397
Stephen Petrick <stephenp1912@gmail.com>
2020-10-13 01:02:59 -0400

    took time_objects class definition for Days and put it into its header file.

---
+++ time_objects.h
@@ -0,0 +1,15 @@
+#ifndef TIME_OBJECTS_H
+#define TIME_OBJECTS_h
+
+class Days {
+   
+   public:
+      int getGrandTotal ( void )
+
+      Days(); //constructor
+
+   private:
+      int grandTotal;
+
+}
+#endif
